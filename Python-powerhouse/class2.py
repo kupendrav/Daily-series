8636a8631b33e37f6689520761076b12d6c5bfc7
kupendrav <kupendrav99@gmail.com>
2025-11-09 18:57:10 +0530

    Add initial configuration for VSCode and Python naming conventions examples

---
+++ Python-powerhouse/class2.py
@@ -0,0 +1,25 @@
+#naming conventions 
+#snake_case 
+first_name = "John"
+#camelCase
+firstName = "John"
+#PascalCase
+FirstName = "John"
+
+#data types
+
+#int 
+x = 1 
+print(type(x))
+
+#float
+x = 1.0
+print(type(x))
+
+#complex
+x = 1j
+print(type(x))
+
+#string
+x = "hello"
+print(type(x))
