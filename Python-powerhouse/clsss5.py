6657d4d657aebac9415d62ba2118d97d3b0beec3
kupendrav <kupendrav99@gmail.com>
2025-11-12 20:58:31 +0530

    Add clsss5.py with examples of comparison and logical operators

---
+++ Python-powerhouse/clsss5.py
@@ -0,0 +1,18 @@
+#comparision operator 
+# == ,!=,>,<,>=,<=
+x = 10
+y = 20
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+
+#logical operator
+# and , or ,not
+x = 10
+y = 20
+print(x==y and x>y)
+print(x==y or x>y)
+print(not(x==y or x>y))
