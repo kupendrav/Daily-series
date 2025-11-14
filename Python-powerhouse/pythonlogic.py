9203ead3eb119847363668cc3c6623b1ae5246f2
kupendrav <kupendrav99@gmail.com>
2025-11-14 21:45:03 +0530

    Add examples for number parity, age eligibility, and weekday identification in pythonlogic.py

--- Python-powerhouse/pythonlogic.py
+++ Python-powerhouse/pythonlogic.py
@@ -20,3 +20,41 @@
 #     print("wrong input only provide m or f")
 
 # 4
+# num = int(input("Enter the number:  "))
+
+# if num % 2 == 0:
+#     print(f"{num} is even number")
+# else:
+#     print(f"{num} is odd number")
+
+# 5
+# name = input("Enter your name:- ")
+# age = int(input(f"{name} enter your age:- "))
+
+# if age >= 18:
+#     print(f"{name} you are eligible to vote")
+# elif age < 18:
+#     print(f"{name} you are not eligible to vote now, you are eligable after {18-age} years")
+# else:
+#     print("wrong input")
+
+# 6
+# weak = int(input("tell your weak day(1-7):- "))
+
+# if weak == 1:
+#     print("monday")
+# elif weak == 2:
+#     print("tuesday")
+# elif weak == 3:
+#     print("wednesday")
+# elif weak == 4:
+#     print("thursday")
+# elif weak == 5:
+#     print("friday")
+# elif weak == 6:
+#     print("saturday")
+# elif weak == 7:
+#     print("sunday")
+# else:
+#     print("wrong input")
+
