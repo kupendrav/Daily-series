d494f517fea537a0e63c3a4230da3b4350b55b63
kupendrav <kupendrav99@gmail.com>
2025-11-14 21:05:43 +0530

    Refactor pythonlogic.py to comment out example code for number comparison and gender input

--- Python-powerhouse/pythonlogic.py
+++ Python-powerhouse/pythonlogic.py
@@ -1,9 +1,22 @@
-a = int(input("Enter the first number:- "))
-b = int(input("Enter the second number:- "))
+# 1
+# a = int(input("Enter the first number:- "))
+# b = int(input("Enter the second number:- "))
 
-if a > b:
-    print("a is greater than b")
-elif b > a:
-    print("b is greater than a")
-else:
-    print("a and b are equal")
+# if a > b:
+#     print("a is greater than b")
+# elif b > a:
+#     print("b is greater than a")
+# else:
+#     print("a and b are equal")
+
+# 2 and 3
+# a = input("Enter your gender:- ")
+
+# if a == 'm' or a == 'M':
+#     print("Hello sir")
+# elif a == 'f' or a == 'F':
+#     print("Hello ma'am")
+# else:
+#     print("wrong input only provide m or f")
+
+# 4
