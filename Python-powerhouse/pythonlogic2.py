0f5ed7c48167ed8ca6ac2ae437bd4e7ffd3cff21
kupendrav <kupendrav99@gmail.com>
2025-11-19 16:04:25 +0530

    Implement prime number checking functionality in pythonlogic2.py

--- Python-powerhouse/pythonlogic2.py
+++ Python-powerhouse/pythonlogic2.py
@@ -68,3 +68,23 @@
 #     power = power * a
 
 # print(power)
+
+#10
+# n = int(input("enter the number: "))
+# count = 0
+# for i in range(1,n+1):
+#     if n % i == 0:
+#         count = count + 1
+# if count == 2:
+#     print(f"{n} is prime number")
+# else:
+#     print(f"{n} is not a prime number")
+
+n = int(input("enter the number: "))
+
+for i in range(2,n):
+    if n % i ==0:
+        print(f"{n} is not a prime number")
+        break
+else:
+    print(f"{n} is a prime number")
