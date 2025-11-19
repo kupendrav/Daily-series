4b2c237ae55bf5bc5aafa3f56118d5ebc1f044c3
kupendrav <kupendrav99@gmail.com>
2025-11-19 09:26:32 +0530

    Add initial implementation of number printing tasks in class7.py

---
+++ Python-powerhouse/class7.py
@@ -0,0 +1,12 @@
+#task1
+#print numbers from 10 to 40
+# for i in range(10,41):
+#     print(i)
+
+#print numbers from -10 to 20
+# for i in range(-10,21):
+#     print(i)
+
+#print numbers from 34 to 5
+for i in range(34,4,-1):
+    print(i)
